6d82e0a840a3248e9257a3d183bd4860584b2228
Bunk <jd.courtoy@gmail.com>
2014-12-20 22:46:11 -0600

    Updated styling and added a logo.

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,9 +270,9 @@ module.exports = function (grunt) {
         ],
         // This is so we update image references in our ng-templates
         patterns: {
-          js: [
-            [/(assets\/images\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']
-          ]
+            js: [
+                [/(assets\/images\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, 'Update the JS to reference our revved images']
+            ]
         }
       }
     },
